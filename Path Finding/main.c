27b12ab346a8776517b41e1c969f9ab6b4d5d7d2
courtneyum <bonnerc3@myumanitoba.ca>
2017-04-03 01:56:10 -0500

    Building and Visualization of Quadtree
    Algorithm for determining if an object is contained in a region needs
    some work, but it's fast.

--- Path Finding/main.c
+++ Path Finding/main.c
@@ -10,6 +10,7 @@ typedef struct square
 {
 	Point center;
 	float length;
+	float width;
 } Square;
 typedef struct map
 {
@@ -18,20 +19,183 @@ typedef struct map
 	int numSquares;
 	Square* squares;
 } Map;
+typedef struct node
+{
+	struct node* tl_child;
+	struct node* tr_child;
+	struct node* bl_child;
+	struct node* br_child;
+	int depth;
+} Node;
+typedef struct queue
+{
+	Node* front;
+	int length;
+}Queue;
 
-int** qdDecomp(Map* map);
+
+void makeQT(Node* curr, Map* map, Point loc, float l, float w);
+char contains_object(Map* map, Point loc, float l, float w);
 Map * makeMap(char* filename);
 void printMap(Map * map);
+void outputTree(Node* node, FILE *fp, Point point, float l, float w);
+
+//Maximum depth of the quadtree
+const int qt_threshold = 7;
 
 int main(int argc, char** args)
 {
 	Map* map = makeMap("map.txt");
 	printMap(map);
+	Node* root = (Node *)malloc(sizeof(Node));
+	root->depth = 0;
+	makeQT(root, map, map->min, map->max.x - map->min.x, map->max.y - map->min.y);
+
+	FILE* fp = fopen("tree.txt", "w+");
+	outputTree(root, fp, map->min, map->max.x - map->min.x, map->max.y - map->min.y);
+	fclose(fp);
 }
 
-int** qdDecomp(Map* map)
+void makeQT(Node* curr, Map* map, Point loc, float l, float w)
 {
-	return NULL;
+	if (curr->depth == qt_threshold)
+	{
+		curr->tl_child = NULL;
+		curr->tr_child = NULL;
+		curr->bl_child = NULL;
+		curr->br_child = NULL;
+		return;
+	}
+
+	if (contains_object(map, loc, l, w))
+	{
+		//Top left child
+		curr->tl_child = (Node *)malloc(sizeof(Node));
+		curr->tl_child->depth = curr->depth + 1;
+		Point tl_loc;
+		tl_loc.x = loc.x;
+		tl_loc.y = loc.y + w/2;
+		makeQT(curr->tl_child, map, tl_loc, l/2, w/2);
+
+		//Top right child
+		curr->tr_child = (Node *)malloc(sizeof(Node));
+		curr->tr_child->depth = curr->depth + 1;
+		Point tr_loc;
+		tr_loc.x = loc.x + l/2;
+		tr_loc.y = loc.y + w/2;
+		makeQT(curr->tr_child, map, tr_loc, l/2, w/2);
+
+		//Bottom left child
+		curr->bl_child = (Node *)malloc(sizeof(Node));
+		curr->bl_child->depth = curr->depth + 1;
+		Point bl_loc;
+		bl_loc.x = loc.x;
+		bl_loc.y = loc.y;
+		makeQT(curr->bl_child, map, bl_loc, l/2, w/2);
+
+		//Bottom right child
+		curr->br_child = (Node *)malloc(sizeof(Node));
+		curr->br_child->depth = curr->depth + 1;
+		Point br_loc;
+		br_loc.x = loc.x + l/2;
+		br_loc.y = loc.y;
+		makeQT(curr->br_child, map, br_loc, l/2, w/2);
+	}
+	else
+	{
+		curr->tl_child = NULL;
+		curr->tr_child = NULL;
+		curr->bl_child = NULL;
+		curr->br_child = NULL;
+	}
+}
+char contains_object(Map* map, Point loc, float l, float w)
+{
+	char leftBounded;
+	char rightBounded;
+	char topBounded;
+	char bottomBounded;
+	Point obj_min;
+	Point obj_max;
+	Square curr;
+	for (int i = 0; i < map->numSquares; i++)
+	{
+		curr = map->squares[i];
+		obj_max.x = curr.center.x + curr.length/2;
+		obj_max.y = curr.center.y + curr.width/2;
+		obj_min.x = curr.center.x - curr.length/2;
+		obj_min.y = curr.center.y - curr.width/2;
+		
+		//left bounded?
+		if (obj_min.x >= loc.x && obj_min.x <= loc.x + l)
+		{
+			if(obj_max.y >= loc.y + w && obj_min.y <= loc.y + w)
+			{
+				leftBounded = 1;
+			}
+			else if (obj_min.y <= loc.y && obj_max.y >= loc.y)
+			{
+				leftBounded = 1;
+			}
+			else if (obj_min.y >= loc.y && obj_max.y <= loc.y + w)
+			{
+				leftBounded = 1;
+			}
+		}
+		//right bounded?
+		else if (obj_max.x >= loc.x && obj_max.x <= loc.x + l)
+		{
+			if(obj_max.y >= loc.y + w && obj_min.y <= loc.y + w)
+			{
+				rightBounded = 1;
+			}
+			else if (obj_min.y <= loc.y && obj_max.y >= loc.y)
+			{
+				rightBounded = 1;
+			}
+			else if (obj_min.y >= loc.y && obj_max.y <= loc.y + w)
+			{
+				rightBounded = 1;
+			}
+		}
+		//top bounded?
+		else if (obj_max.y >= loc.y && obj_max.y <= loc.y + w)
+		{
+			if (obj_min.x <= loc.x && obj_max.x >= loc.x)
+			{
+				topBounded = 1;
+			}
+			else if (obj_max.x >= loc.x + l && obj_min.x <= loc.x + l)
+			{
+				topBounded = 1;
+			}
+			else if (obj_min.x >= loc.x && obj_max.x <= loc.x + l)
+			{
+				topBounded = 1;
+			}
+		}
+		//bottom bounded?
+		else if (obj_min.y >= loc.y && obj_min.y <= loc.y + w)
+		{
+			if (obj_min.x <= loc.x && obj_max.x >= loc.x)
+			{
+				bottomBounded = 1;
+			}
+			else if (obj_max.x >= loc.x + l && obj_min.x <= loc.x + l)
+			{
+				bottomBounded = 1;
+			}
+			else if (obj_min.x >= loc.x && obj_max.x <= loc.x + l)
+			{
+				bottomBounded = 1;
+			}
+		}
+	}
+	if (leftBounded || rightBounded || topBounded || bottomBounded)
+	{
+		return 1;
+	}
+	return 0;
 }
 
 Map * makeMap(char* filename)
@@ -64,8 +228,10 @@ Map * makeMap(char* filename)
 		map->squares[count].center.y = num;
 		fscanf(fp, "%f", &num);
 		map->squares[count].length = num;
+		map->squares[count].width = num; //FOR NOW ASSUME SQUARE OBSTACLES
 		count++;
 	}
+	fclose(fp);
 	return map;
 }
 void printMap(Map * map)
@@ -77,6 +243,38 @@ void printMap(Map * map)
 	{
 		printf("Center Coord: (%f,%f)\n", map->squares[i].center.x, map->squares[i].center.y);
 		printf("Length: %f\n", map->squares[i].length);
+		//printf("Width: %f\n", map->squares[i].width);
+	}
+}
+void outputTree(Node* node, FILE *fp, Point point, float l, float w)
+{
+	if (node == NULL)
+	{
+		return;
+	}
+	if (node->tl_child != NULL)
+	{
+		Point newPoint;
+
+		newPoint.x = point.x;
+		newPoint.y = point.y + l/2;
+		outputTree(node->tl_child, fp, newPoint, l/2, w/2);
+
+		newPoint.x = point.x + l/2;
+		newPoint.y = point.y + l/2;
+		outputTree(node->tr_child, fp, newPoint, l/2, w/2);
+
+		newPoint.x = point.x;
+		newPoint.y = point.y;
+		outputTree(node->bl_child, fp, newPoint, l/2, w/2);
+
+		newPoint.x = point.x + l/2;
+		newPoint.y = point.y;
+		outputTree(node->br_child, fp, newPoint, l/2, w/2);
+	}
+	else
+	{
+		fprintf(fp, "%f %f %f %f\n", point.x, point.y, l, w);
 	}
 }
 
--- Path Finding/showMap.m
+++ Path Finding/showMap.m
@@ -1,7 +1,6 @@
 fileID = fopen('map.txt', 'r');
 formatSpec = '%f';
 data = fscanf(fileID,formatSpec);
-a = 0;
 x_min = data(1);
 x_max = data(2);
 y_min = data(3);
@@ -13,5 +12,17 @@ for i=6:3:size(data,1) - 2
     x = data(i) - data(i+2)/2;
     y = data(i+1) - data(i+2)/2;
     w = data(i+2);
-    rectangle('Position', [x y w w]);
+    rectangle('Position', [x y w w], 'FaceColor', [0 0 1]);
+end;
+
+fileID = fopen('tree.txt', 'r');
+formatSpec = '%f';
+data = fscanf(fileID, formatSpec);
+
+for i=1:4:size(data,1)
+    x = data(i);
+    y = data(i + 1);
+    l = data(i + 2);
+    w = data(i +3);
+    rectangle('Position', [x y l w]);
 end;
