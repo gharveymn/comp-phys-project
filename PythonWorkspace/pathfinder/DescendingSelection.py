7bef4b2a5f3f331abaa211f747285f3fc2e4c4cd
Gene Harvey <gharveymn@gmail.com>
2017-04-14 20:39:11 -0500

    more stability to DS, really it's just a lookup table but I'm still proud of it

--- PythonWorkspace/pathfinder/DescendingSelection.py
+++ PythonWorkspace/pathfinder/DescendingSelection.py
@@ -7,7 +7,17 @@ import matplotlib.lines as lines
 import time
 
 
-def desselect(mdict, order, point):
+def desselect(mdict, order, point, bounds):
+	delx = bounds[1]-bounds[0]
+	dely = bounds[3]-bounds[2]
+
+	newpoint = (10.*point[0]/delx, 10.*point[1]/dely)
+	
+	return evaldesselect(mdict, order, newpoint)
+pass
+
+
+def evaldesselect(mdict, order, point):
 
 	if not order:
 		return mdict[(int(point[0]),int(point[1]))]
@@ -15,32 +25,35 @@ def desselect(mdict, order, point):
 		pxFrac,pxInt = modf(point[0])
 		pyFrac,pyInt = modf(point[1])
 
-		return desselect(mdict[(int(pxInt),int(pyInt))], order-1, (10.*pxFrac,10*pyFrac))
+		return evaldesselect(mdict[(int(pxInt),int(pyInt))], order-1, (10.*pxFrac,10*pyFrac))
 	pass
 
 pass
 
 
-def prepare(points, order, bounds):
+def prepare(pointArray, order, bounds):
 
 	if not order:
-
+		#t = time.time()
 		delx = (bounds[1]-bounds[0])/10.
 		dely = (bounds[3]-bounds[2])/10.
 
 		finalmat = {}
 		for i in range(10):
 			for j in range(10):
-				finalmat[(i,j)] = findClosestNode(points,(bounds[0]+(i+1./2)*delx,bounds[2]+(j+1./2)*dely))
+				#finalmat[(i,j)] = findClosestNode(pointArray,(bounds[0]+(i+1./2)*delx,bounds[2]+(j+1./2)*dely))
+				finalmat[(i,j)] = pointArray[findClosestNode2(pointArray,(bounds[0]+(i+.5)*delx,bounds[2]+(j+.5)*dely))]
 			pass
 		pass
+		#print("1: {0}".format(time.time() - t))
 
 		return finalmat
 
 	else:
-
+		#t = time.time()
 		newmat = decimate(bounds)
-		return {(i,j):prepare(points,order-1,newmat[i][j]) for i in newmat for j in newmat}
+		print("Order: {0}, Bounds: {1}".format(order,bounds))
+		return {(i,j):prepare(pointArray,order-1,newmat[i][j]) for i in newmat for j in newmat}
 	pass
 
 pass
@@ -64,29 +77,36 @@ def decimate(bounds):
 	return newmat
 pass
 
-def findClosestNode(points,p):
-	return min(points, key=lambda x: distancesq(x, p))
+def findClosestNode(pointArray,p):
+	return min(pointArray, key=lambda x: distancesq(x, p))
 pass
 
 def distancesq(r1,r2): return (r2[0]-r1[0])**2 + (r2[1]-r1[1])**2
+
+def findClosestNode2(pointArray, p):
+    pointArray = np.asarray(pointArray)
+    deltas = pointArray - p
+    return np.argmin( np.einsum('ij,ij->i', deltas, deltas) )
+pass
+
 
 def tester():
 
 	testingPoint = (1,1)
 
-	bounds = (0.,10.,0.,10.)
-	randArr = list(zip(list(bounds[1]*random.random(20)),list(bounds[3]*random.random(20))))
+	bounds = (0.,5.,0.,10.)
+	randArr = list(zip(list((bounds[1]-bounds[0])*random.random(20)),list((bounds[3]-bounds[2])*random.random(20))))
 	order = 1
 
 	clr = makeColorDict(randArr)
 
 	mdict = prepare(randArr,order,bounds)
-	closest = desselect(mdict,1,testingPoint)
+	closest = desselect(mdict,order,testingPoint,bounds)
 
 	print(closest)
 	print(True) if closest in randArr else print(False)
 
-	package = getScatter(mdict,order,order,clr)
+	package = getScatter(mdict,order,order,clr,bounds)
 	xs,ys,clrs = zip(*package)
 
 	#print(mdict)
@@ -104,12 +124,12 @@ pass
 
 
 def makeRandomArray(numPoints, bounds):
-	return list(zip(list(bounds[1]*random.random(numPoints)),list(bounds[3]*random.random(numPoints))))
+	return list(zip(list((bounds[1]-bounds[0])*random.random(numPoints)),list((bounds[3]-bounds[2])*random.random(numPoints))))
 pass
 
 
 def makeColorDict(pointArray):
-	return {p:(0.5+0.5*random.random(),0.5+0.5*random.random(),0.5+0.5*random.random()) for p in pointArray}
+	return {p:(0.1+0.9*random.random(),0.1+0.9*random.random(),0.1+0.9*random.random()) for p in pointArray}
 pass
 
 
@@ -117,7 +137,7 @@ def drawBucketMap(mdict,order,clr,pointArray):
 	package = getScatter(mdict,order,order,clr)
 	xs,ys,clrs = zip(*package)
 	scatter(xs,ys,c=clrs,s=1)
-	scatter(*zip(*pointArray),c='black')
+	scatter(*zip(*pointArray),c='black',s=2)
 pass
 
 
@@ -127,7 +147,7 @@ def drawFind(mdict, order, point):
 	scatter(*point,c='blue',s=10)
 pass
 
-def getScatter(mdict, persistentorder, order, clr, absPosX = 0, absPosY = 0, ret = []):
+def getScatter(mdict, persistentorder, order, clr, bounds, absPosX = 0, absPosY = 0, ret = []):
 
 	if order == -1:
 		#mdict since last access at mdict[p] will be the value
@@ -135,11 +155,11 @@ def getScatter(mdict, persistentorder, order, clr, absPosX = 0, absPosY = 0, ret
 		return [(absPosX,absPosY,clr[mdict])]
 		#print("({0},{1})".format(absPosX,absPosY))
 	else:
-		mltplr = 10**(order-persistentorder)
+		mltplrx = 10**(order-persistentorder-1)*(bounds[1]-bounds[0])
+		mltplry = 10**(order-persistentorder-1)*(bounds[3]-bounds[2])
 		#TODO make absPoX,Y in middle, just take half of length on order	
-
 		for p in mdict:
-			ret = ret + getScatter(mdict[p], persistentorder, order-1, clr, absPosX+p[0]*mltplr, absPosY+p[1]*mltplr)
+			ret = ret + getScatter(mdict[p], persistentorder, order-1, clr, bounds, absPosX+(p[0])*mltplrx, absPosY+(p[1])*mltplry)
 		pass
 
 		return ret
@@ -159,7 +179,7 @@ pass
 def timeReg(pointArray,points):
 	t1 = time.time()
 	for point in points:
-		findClosestNode(pointArray,point)
+		points[findClosestNode2(points,point)]
 	pass
 	t2 = time.time()
 	return t2-t1
--- PythonWorkspace/pathfinder/HelperMethods.py
+++ PythonWorkspace/pathfinder/HelperMethods.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def distancesq(r1,r2): return (r2[0]-r1[0])**2 + (r2[1]-r1[1])**2
 
 def addtup(i,j): return tuple(map(sum,zip(i,j)))
@@ -5,3 +7,9 @@ def addtup(i,j): return tuple(map(sum,zip(i,j)))
 def eledivtup(i,j): return tuple(map((lambda x: x[0]/x[1]),zip(i,j)))
 
 def scadivtup(t,s): return tuple(map((1./s).__mul__, t))
+
+def findClosestNode(pointArray, p):
+    pointArray = np.asarray(pointArray)
+    deltas = pointArray - p
+    return np.argmin( np.einsum('ij,ij->i', deltas, deltas) )
+pass
