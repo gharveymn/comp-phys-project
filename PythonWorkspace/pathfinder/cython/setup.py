7c9bfbd52c7751a101092a71419ef30a85a74f65
Gene Harvey <gharveymn@gmail.com>
2017-04-20 15:37:42 -0500

    removes usless cython files because I don't like github saying this is an html project

--- PythonWorkspace/pathfinder/cython/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-
-#This doesn't actually work on my machine...
-setup(
-  name = 'pathfinder',
-  ext_modules = cythonize("*.pyx"),
-)
