52485d7e064d8a166cd0f9832c61c110c1b258f7
courtneyum <bonnerc3@myumanitoba.ca>
2017-04-22 18:25:44 -0500

    NOT WORKING CODE

--- PythonWorkspace/pathfinder/ThreadedPartition.py
+++ PythonWorkspace/pathfinder/ThreadedPartition.py
@@ -1,4 +1,5 @@
 import PathOptimization as po
+
 # TODO:
 # input:
 # 	pack
@@ -12,19 +13,16 @@ import PathOptimization as po
 #	dictionary of paths in form 
 # {(startpoint1):{(endpoint1):[(pathpoint1),(pathpoint2),...], (endpoint2):{...}}, (startpoint2):{...}}
 
-def findPaths(pack, lock, dynamicPaths, startPoints, endsPoints):
+def findPaths(pack, pdict, lock, dynamicPaths, startPoints, endPoints):
     gdict = pack[0]
-    pdict = {}
 
-    for i in range (0, len(startPoints) - 1):
+    for i in range (0, len(startPoints)):
         pdict[startPoints[i]] = {}
-        for j in range (0, len(endsPoints) - 1):
-            sp = po.dynamicDijkstra(gdict, startPoints[i], endsPoints[j], dynamicPaths)
+        for j in range (0, len(endPoints)):
+            sp = po.dynamicDijkstra(gdict, lock, startPoints[i], endPoints[j], dynamicPaths)
             path = sp[1]
-            pdict[startPoints[i]] = {endsPoints[j]: path}
+            pdict[startPoints[i]] = {endPoints[j]: path}
         pass
     pass
-
-    return pdict
 pass
 	
