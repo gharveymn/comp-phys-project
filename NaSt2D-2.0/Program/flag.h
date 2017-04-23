26dcb0dfa67f9c5dca306628a56256f153f09753
Gene Harvey <gharveymn@gmail.com>
2017-04-23 02:09:38 -0500

    removed unneeded files, updated the readme

--- NaSt2D-2.0/Program/flag.h
+++
@@ -1,4 +0,0 @@
-//*****************************************************************flag.h
-void INIT_FLAG(char *problem,int **FLAG,int pflag,int imax,int jmax,
-               REAL delx,REAL dely,REAL delxw,REAL delyw,int *ibound,
-               char *vecfile,char *flagfile);
--- NaSt2D/source/boundary.h
+++
@@ -1,8 +0,0 @@
-void SETBCOND(REAL **U,REAL **V,REAL **P,REAL **TEMP,int **FLAG,
-	      int imax,int jmax,int wW,int wE,int wN,int wS);
-
-void SETSPECBCOND(char* problem,REAL **U,REAL **V,REAL **P,REAL **TEMP,
-		  int imax,int jmax,REAL UI, REAL VI);
-
-
-
